89d37a945484e9774ea7d5a6bfb48c795073d927
Corey Anthony <canthony@techtonic.com>
2018-06-25 13:34:57 -0600

    reset js file

--- js/site.js
+++ js/site.js
@@ -1,33 +1,12 @@
-//JS Code Challenge. Find three question marks between two numbers that add up to 10.
-//June 22, 2018
-//Created by Corey Anthony
+var Library = function(){
+  this._bookShelf = new Array();
+};
 
-var QuestionMarks = function(str){
-  var res = str.match(/\d.*?(\d)/g);
-  var trueFlag = false;
-    if (res === null || res[0].length < 3) {  //if there is not one or more ? between two numbers log false.
-      } else {
-        for (var i = 0; i < res.length; i++) {
-          if (res[i].match(/\?/g).length ===3) { //check to see if exactly three ???
-            //Add the first and last number in the string together.
-            if (parseInt(res[i].charAt(0))+parseInt(res[i].slice(-1)) === 10) {
-              trueFlag = true; //If it adds up to 10 log true
-            } else {
-                //If it does not add up to 10 log false
-              if (!trueFlag) {trueFlag = false;}
-            }
-          } else {
-            if (!trueFlag) {trueFlag = false;} //not three ???, log false.
-          }
-        }
-      }
-    console.log(trueFlag);
-  }
+Library.prototype.addBook = function (book) {};
 
-//Senarios to try:
-//QuestionMarks("arrb6???ee?4xxbl5???eee5");
-//QuestionMarks("arrb6??s4xxbl5??g?eee5");
-//QuestionMarks("arr");
-//QuestionMarks("bb6?9c");
-//QuestionMarks("bb69c");
-//QuestionMarks("bcc?7??ccc?3tt1??????5");
+Library.prototype.removeBookbyTttle = function (title) {};
+
+document.addEventListener("DOMContentLoaded", function() {
+  window.gLibrary = new Library();
+  window.bookOne = new Book("IT","Stephan King", 800, new Date("12","24","1987"))
+});
